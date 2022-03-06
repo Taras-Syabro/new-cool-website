9ec6160f79d6569a56279c873df45e211296d2a0
taras <tarassyabro@gmail.com>
2022-03-06 17:58:14 +0100

    my site

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
+
+    bootstrap.init_app(app)
 
     from app.views import main_bp
     app.register_blueprint(main_bp)
